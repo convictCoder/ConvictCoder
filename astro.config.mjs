9333c379b326ccbdb8766c9aaffb1c51b3c512f4
Alana <alanaandres@gmail.com>
2026-06-24 18:45:31 -0700

    Remove unused React integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,3 @@
 import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
 
-export default defineConfig({
-  integrations: [react()],
-  assets: {
-    // Enable Astro's built-in image optimization
-    // You can configure domains, cache, etc. here if needed
-  },
-});
+export default defineConfig({});
